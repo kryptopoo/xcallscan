ca2d8d91057f4f23cd532fb298c7677b67c55b6b
kryptopoo <kryptopoo@gmail.com>
2024-02-01 22:21:51 +0700

    feat/explorer-add-network-links

--- explorer/components/message-list.js
+++ explorer/components/message-list.js
@@ -7,7 +7,7 @@ export default async function MessageList({ data, meta, showPagination }) {
     return (
         <div className="py-2">
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <div className="table w-full border-collapse">
+                <div className="table w-full border-collapse min-h-48 bg-white">
                     <div className="table-header-group">
                         <div className="table-row uppercase text-left bg-gray-50">
                             <div className="table-cell px-2 py-1 xl:px-6 xl:py-3">Status</div>
@@ -42,7 +42,7 @@ export default async function MessageList({ data, meta, showPagination }) {
                     <Pagination totalPages={meta.pagination.total} pageNumber={meta.pagination.number} pageSize={meta.pagination.size} />
                 ) : (
                     <div className="py-4 px-4 text-center hover:underline underline-offset-2">
-                        <Link href={`/messages`}>VIEW ALL MESSAGES</Link>
+                        {data.length == 0 ? <div>NO MESSAGES</div> : <Link href={`/messages`}>VIEW ALL MESSAGES</Link>}
                     </div>
                 )}
             </div>
