e36f1f41f239d691c68b8eace76edb3eae6bfc3c
kryptopoo <kryptopoo@gmail.com>
2024-06-03 18:08:55 +0700

    fix(indexer): missing CallExecuted event

--- indexer/index.ts
+++ indexer/index.ts
@@ -9,23 +9,39 @@ async function run() {
     logger.info('start indexing networks', networks)
 
     // fetch data between networks
-    cron.schedule('0 */2 * * * *', async () => {
-        // TODO: review ETIMEDOUT issue
+    // fetch ICON networks
+    cron.schedule('0 */1 * * * *', async () => {
         await Promise.all(
-            networks.map((network) => {
+            [NETWORK.ICON, NETWORK.HAVAH].map((network) => {
+                return fetch(network)
+            })
+        )
+    })
+    // fetch evm networks
+    cron.schedule('15 */1 * * * *', async () => {
+        await Promise.all(
+            [NETWORK.AVAX, NETWORK.BSC, NETWORK.BASE, NETWORK.ARBITRUM, NETWORK.OPTIMISM, NETWORK.ETH2].map((network) => {
+                return fetch(network)
+            })
+        )
+    })
+    // fetch ibc networks
+    cron.schedule('30 */1 * * * *', async () => {
+        await Promise.all(
+            [NETWORK.IBC_ARCHWAY, NETWORK.IBC_INJECTIVE, NETWORK.IBC_NEUTRON].map((network) => {
                 return fetch(network)
             })
         )
     })
 
     // sync new messages
-    cron.schedule('30 */2 * * * *', async () => {
+    cron.schedule('0 */1 * * * *', async () => {
         const syncer = new Syncer(networks)
         await syncer.syncNewMessages()
     })
 
     // sync pending/unfinished messages
-    cron.schedule('55 */2 * * * *', async () => {
+    cron.schedule('45 */1 * * * *', async () => {
         const syncer = new Syncer(networks)
         await syncer.syncUnfinishedMessages()
     })
