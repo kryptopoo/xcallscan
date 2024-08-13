98a3c839c81b6a1951b64c3ccff20e47d2a31b1f
kryptopoo <kryptopoo@gmail.com>
2024-08-13 21:28:10 +0700

    feat: msg action types

--- indexer/modules/analyzer/Analyzer.ts
+++ indexer/modules/analyzer/Analyzer.ts
@@ -27,11 +27,11 @@ export class Analyzer {
         this.subscriber.events.on('error', (error) => {
             logger.error(`[analyzer] subscriber error ${error.message}`)
         })
-        this.subscriber.notifications.on('message', (data: any) => {
-            logger.info(`[analyzer] subscriber message ${JSON.stringify(data)}`)
+        this.subscriber.notifications.on('message', async (data: any) => {
+            // logger.info(`[analyzer] subscriber message ${JSON.stringify(data)}`)
             if (data.src_tx_hash && data.dest_tx_hash) {
-                logger.info(`[analyzer] sn:${data.sn} ${data.src_network} ${data.src_tx_hash} -> ${data.dest_network} ${data.dest_tx_hash}`)
-                const msgAcction = actionParser.parseMgsAction(data.src_network, data.src_tx_hash, data.dest_network, data.dest_tx_hash)
+                logger.info(`[analyzer] id:${data.id} sn:${data.sn} ${data.src_network} ${data.src_tx_hash} -> ${data.dest_network} ${data.dest_tx_hash}`)
+                const msgAcction = await actionParser.parseMgsAction(data.src_network, data.src_tx_hash, data.dest_network, data.dest_tx_hash)
                 logger.info(`[analyzer] msgAcction`, msgAcction) 
             }
         })
