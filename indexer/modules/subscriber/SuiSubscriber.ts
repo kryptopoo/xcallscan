a1ae8a2aa6bd25c8b52de3bf3ad0e98afe50283a
kryptopoo <kryptopoo@gmail.com>
2024-09-29 21:50:00 +0700

    feat: sui subscriber

--- indexer/modules/subscriber/SuiSubscriber.ts
+++ indexer/modules/subscriber/SuiSubscriber.ts
@@ -1,17 +1,10 @@
-import { WebSocket } from 'ws'
 import { ISubscriber, ISubscriberCallback } from '../../interfaces/ISubcriber'
 import { CONTRACT, EVENT, NETWORK, RPC_URL, RPC_URLS, SUBSCRIBER_INTERVAL, WSS } from '../../common/constants'
 import { subscriberLogger as logger } from '../logger/logger'
-import { IbcDecoder } from '../decoder/IbcDecoder'
-import { v4 as uuidv4 } from 'uuid'
-import { toHex } from '@cosmjs/encoding'
-import { sha256 } from '@cosmjs/crypto'
-import { StargateClient } from '@cosmjs/stargate'
 import { EventLog, EventLogData } from '../../types/EventLog'
-import { IDecoder } from '../../interfaces/IDecoder'
-import { getFullnodeUrl, QueryEventsParams, SuiClient } from '@mysten/sui/client'
 import AxiosCustomInstance from '../scan/AxiosCustomInstance'
 import { SuiDecoder } from '../decoder/SuiDecoder'
+import { retryAsync } from 'ts-retry'
 
 export class SuiSubscriber implements ISubscriber {
     network: string = NETWORK.SUI
@@ -61,66 +54,83 @@ export class SuiSubscriber implements ISubscriber {
         this.decoder = new SuiDecoder()
     }
 
-    subscribe(callback: ISubscriberCallback): void {
+    async subscribe(callback: ISubscriberCallback): Promise<void> {
         logger.info(`${this.network} connect ${WSS[this.network][0]}`)
         logger.info(`${this.network} listen events on ${JSON.stringify(this.contractAddress)}`)
 
-        this.queryTxBlocks('', true, 1).then((res) => {
-            let nextCursor = res.nextCursor
-            let hasNextPage = res.hasNextPage
+        const res = await retryAsync(
+            async () => {
+                return await this.queryTxBlocks('', true, 1)
+            },
+            { delay: 1000, maxTry: 3 }
+        )
+        let nextCursor = res.nextCursor
+        let hasNextPage = res.hasNextPage
 
-            const intervalTask = setInterval(() => {
+        const task = () => {
+            const intervalId = setInterval(async () => {
                 try {
                     if (hasNextPage && nextCursor) {
-                        this.queryTxBlocks(nextCursor, false).then(async (txsRes) => {
-                            console.log('txsRes', txsRes)
-                            if (txsRes?.nextCursor) nextCursor = txsRes.nextCursor
-                            hasNextPage = txsRes.hasNextPage
-                            const txs = txsRes?.data?.filter((t: any) => t.events?.length > 0) ?? []
-                            for (let i = 0; i < txs.length; i++) {
-                                const tx = txs[i]
-                                if (tx) {
-                                    const eventsOfTxDetail: any[] = tx.events ?? []
-                                    const eventNames = Object.values(EVENT)
-                                    for (let j = 0; j < eventNames.length; j++) {
-                                        const eventName = eventNames[j]
-                                        const eventLog: any = eventsOfTxDetail.find((e) => {
-                                            // e.g: e.type = 0x25f664e39077e1e7815f06a82290f2aa488d7f5139913886ad8948730a98977d::main::CallMessage
-                                            const eventNameOfTx = e.type.split('::').pop()
-                                            return eventName === eventNameOfTx
-                                        })
-                                        const decodeEventLog = await this.decoder.decodeEventLog(eventLog?.parsedJson, eventName)
+                        const txsRes = await retryAsync(
+                            async () => {
+                                return this.queryTxBlocks(nextCursor, false)
+                            },
+                            { delay: 1000, maxTry: 3 }
+                        )
 
-                                        if (decodeEventLog) {
-                                            const txFee =
-                                                tx.effects.gasUsed.storageCost - tx.effects.gasUsed.storageRebate + tx.effects.gasUsed.computationCost
-                                            const log: EventLog = {
-                                                // txRaw: tx.transaction,
-                                                blockNumber: Number(tx.checkpoint),
-                                                blockTimestamp: Math.floor(new Date(Number(tx.timestampMs)).getTime() / 1000),
-                                                txHash: tx.digest,
-                                                txFrom: tx.transaction.data.sender,
-                                                // recipient could be empty
-                                                txTo: tx.balanceChanges.find((b: any) => b.owner.AddressOwner != tx.transaction.data.sender)?.owner
-                                                    .AddressOwner,
-                                                txFee: txFee.toString(),
-                                                // txValue: tx.value.toString(),
-                                                eventName: eventName,
-                                                eventData: decodeEventLog
-                                            }
+                        if (txsRes?.nextCursor) nextCursor = txsRes.nextCursor
+                        hasNextPage = txsRes.hasNextPage
 
-                                            callback(log)
+                        const txs = txsRes?.data?.filter((t: any) => t.events?.length > 0) ?? []
+                        for (let i = 0; i < txs.length; i++) {
+                            const tx = txs[i]
+                            if (tx) {
+                                const eventsOfTxDetail: any[] = tx.events ?? []
+                                const eventNames = Object.values(EVENT)
+                                for (let j = 0; j < eventNames.length; j++) {
+                                    const eventName = eventNames[j]
+                                    const eventLog: any = eventsOfTxDetail.find((e) => {
+                                        // e.g: e.type = 0x25f664e39077e1e7815f06a82290f2aa488d7f5139913886ad8948730a98977d::main::CallMessage
+                                        const eventNameOfTx = e.type.split('::').pop()
+                                        return eventName === eventNameOfTx
+                                    })
+                                    const decodeEventLog = await this.decoder.decodeEventLog(eventLog?.parsedJson, eventName)
+
+                                    if (decodeEventLog) {
+                                        const txFee =
+                                            tx.effects.gasUsed.storageCost - tx.effects.gasUsed.storageRebate + tx.effects.gasUsed.computationCost
+                                        const log: EventLog = {
+                                            // txRaw: tx.transaction,
+                                            blockNumber: Number(tx.checkpoint),
+                                            blockTimestamp: Math.floor(new Date(Number(tx.timestampMs)).getTime() / 1000),
+                                            txHash: tx.digest,
+                                            txFrom: tx.transaction.data.sender,
+                                            // recipient could be empty
+                                            txTo: tx.balanceChanges.find((b: any) => b.owner.AddressOwner != tx.transaction.data.sender)?.owner
+                                                .AddressOwner,
+                                            txFee: txFee.toString(),
+                                            // txValue: tx.value.toString(),
+                                            eventName: eventName,
+                                            eventData: decodeEventLog
                                         }
+
+                                        callback(log)
                                     }
                                 }
                             }
-                        })
+                        }
                     }
                 } catch (error) {
                     logger.error(`clear interval task ${JSON.stringify(error)}`)
-                    clearInterval(intervalTask)
+                    clearInterval(intervalId)
+
+                    // retry with another task
+                    task()
                 }
             }, SUBSCRIBER_INTERVAL)
-        })
+        }
+
+        // run task
+        task()
     }
 }
