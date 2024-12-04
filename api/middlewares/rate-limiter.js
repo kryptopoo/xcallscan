5508a9440987d62e79bf01f0b1c1cbdae1e1e84c
kryptopoo <44108463+kryptopoo@users.noreply.github.com>
2024-12-04 13:13:13 +0700

    feat: api rate limit config (#70)

--- api/middlewares/rate-limiter.js
+++ api/middlewares/rate-limiter.js
@@ -1,8 +1,9 @@
 const rateLimit = require('express-rate-limit')
+const { RATE_LIMIT } = require('./../constants')
 
 const apiRateLimiter = rateLimit({
     windowMs: 1000, // 1 second
-    max: 5,
+    max: RATE_LIMIT,
     message: 'Too many requests'
 })
 
